c2774a101edd10de6f89aa5810aee9494a83d522
kewserseid <kewerseid91@gmail.com>
2024-09-02 16:20:07 +0300

    task one updating client3 ration prices

--- client_test.py
+++ client_test.py
@@ -19,6 +19,15 @@ class ClientTest(unittest.TestCase):
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getDataPoint_bidEqualsAsk(self):
+    quotes = [
+      {'top_ask': {'price': 120.0, 'size': 50}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.0, 'size': 100}, 'id': '0.109974697771', 'stock': 'ABC'}
+    ]
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(stock, 'ABC')
+    self.assertEqual(bid_price, 120.0)
+    self.assertEqual(ask_price, 120.0)
+    self.assertEqual(price, 120.0)
 
 
 if __name__ == '__main__':
